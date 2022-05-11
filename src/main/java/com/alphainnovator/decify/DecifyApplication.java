4e3179bbd105754f45731a8da179082798673e8c
Confidence-bassey <confidencebass2@gmail.com>
2022-05-11 22:21:57 +0100

    Decify project

---
+++ src/main/java/com/alphainnovator/decify/DecifyApplication.java
@@ -0,0 +1,13 @@
+package com.alphainnovator.decify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alphainnovator/decify/DecifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphainnovator.decify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
