9691e3b0f0bf2ad48a110e1e4992516605b2b0fc
Confidence-bassey <confidencebass2@gmail.com>
2022-05-16 18:34:32 +0100

    Mapped customerEntity to its db table

--- src/main/java/com/alphainnovator/decify/core/Entities/CustomerEntity.java
+++ src/main/java/com/alphainnovator/decify/core/Entities/CustomerEntity.java
@@ -1,33 +1,44 @@
 package com.alphainnovator.decify.core.Entities;
 
 
-//import javax.persistence.Entity;
-//import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
-//@Table(name = "customer")
-//@Entity
+@Table(name = "customer")
+@Entity
 @NoArgsConstructor
 @ToString
 public class CustomerEntity {
     
+    @Column(name = "firstname")
     String firstName;
+
+    @Column(name = "lastname")
     String lastName;
+
+    @Column(name = "email")
     String email;
+
+    @Column(name = "phone_number")
     String phoneNumber;
+
+    @Column(name = "order_id")
     int order_id;
+
+    @Column(name = "payment_id")
+    int payment_id;
     
     public CustomerEntity(String firstName, String lastName, String email, String phoneNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.phoneNumber = phoneNumber;
-    }
-
-    
+    } 
     
 }
