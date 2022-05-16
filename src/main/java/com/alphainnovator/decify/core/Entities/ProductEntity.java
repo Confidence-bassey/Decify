3c1c66f9da2cad46d1389058990c0a4141eb55cb
Confidence-bassey <confidencebass2@gmail.com>
2022-05-16 18:35:30 +0100

    Mapped productEntity to its database table

--- src/main/java/com/alphainnovator/decify/core/Entities/ProductEntity.java
+++ src/main/java/com/alphainnovator/decify/core/Entities/ProductEntity.java
@@ -1,25 +1,36 @@
 package com.alphainnovator.decify.core.Entities;
 
-//import javax.persistence.Entity;
-//import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-//@Entity
-//@Table(name = "product")
+@Entity
+@Table(name = "product")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class ProductEntity {
     
+    @Column(name = "item_name")
     String itemName;
+
+    @Column(name = "description")
     String description;
+
+    @Column(name = "item_id")
     long itemId;
+
+    @Column(name = "item_price")
     double ItemPrice;
+
+    @Column(name = "admin_id")
+    int Admin_Id;
 
     public ProductEntity(String itemName, String description, double itemPrice) {
         this.itemName = itemName;
