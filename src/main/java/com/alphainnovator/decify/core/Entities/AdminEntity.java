5e5e9ef3f844249e7728ec80ddbb668e87cd2633
Confidence-bassey <confidencebass2@gmail.com>
2022-05-16 18:33:41 +0100

    mapped AdminEntity to its database table

--- src/main/java/com/alphainnovator/decify/core/Entities/AdminEntity.java
+++ src/main/java/com/alphainnovator/decify/core/Entities/AdminEntity.java
@@ -1,28 +1,44 @@
 package com.alphainnovator.decify.core.Entities;
 
-//import javax.persistence.Entity;
-//import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-//@Entity
-//@Table(name = "admin")
+@Entity
+@Table(name = "admin")
 @Data
 @NoArgsConstructor
 @ToString
 public class AdminEntity {
     
-    int Id;
+
+    @Column(name = "admin_id")
     int AdminId;
 
+    @Column(name = "firstname")
     String firstName;
+
+    @Column(name = "lastname")
 	String lastName;
+
+    @Column(name = "email")
 	String email;
+
+    @Column(name = "phone_number")
 	String phoneNo;
+
+    @Column(name = "payment_id")
     int payment_id;
+
+    @Column(name = "product_id")
     int product_id;
+
+    @Column(name = "service_id")
+    int service_id;
 
     
     public AdminEntity(String firstName, String lastName, String email, String phoneNo) {
